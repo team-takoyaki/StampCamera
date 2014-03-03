abbb57f012c2dfa4ded9afb2d808882383f6d453
Kashima Takumi <kodokurider@gmail.com>
2014-03-03 22:39:21 +0900

    スタンプ一覧を作成中

---
+++ illustCamera/illustCamera/StampListViewController.h
@@ -0,0 +1,13 @@
+//
+//  StampListViewController.h
+//  illustCamera
+//
+//  Created by Kashima Takumi on 2014/03/02.
+//  Copyright (c) 2014年 TEAM TAKOYAKI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StampListViewController : UIViewController
+
+@end
---
+++ illustCamera/illustCamera/StampListViewController.m
@@ -0,0 +1,38 @@
+//
+//  StampListViewController.m
+//  illustCamera
+//
+//  Created by Kashima Takumi on 2014/03/02.
+//  Copyright (c) 2014年 TEAM TAKOYAKI. All rights reserved.
+//
+
+#import "StampListViewController.h"
+
+@interface StampListViewController ()
+
+@end
+
+@implementation StampListViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
